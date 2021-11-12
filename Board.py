f3c67bcac79574cbc2e58ae924cf8754c400aee8
apmolina <apmolina@iu.edu>
2021-11-12 13:11:58 -0500

    Cell and CellState

--- Board.py
+++ Board.py
@@ -1,1 +1,20 @@
+from enum import Enum
+class CellState(Enum):
+    UNKNOWN = 2
+    VOID = 0
+    FILLED = 1
+
+
+
+class Cell:
+
+    def __init__(self, state, rowNumber, columnNumber):
+        self.rowNumber = rowNumber
+        self.columnNumber = columnNumber
+        self.state = CellState(state)
+
+    def changeState(self, value):
+        if(value != self.state):
+            self.state = value
+
 
